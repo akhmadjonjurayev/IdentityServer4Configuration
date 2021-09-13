64c69c78b05fcbc3146ec9a0dd87916b3a5095bc
Akhmadjon <formyproject1999@gmail.com>
2021-09-13 18:41:59 +0500

    first commit

---
+++ Cloudexis/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Cloudexis
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using(var client = new HttpClient())
+            {
+                var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+                if(disco.IsError)
+                {
+                    Console.WriteLine(disco.Issuer);
+                    return;
+                }
+                var tokenResponce = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    Scope = "api1"
+                });
+                if(tokenResponce.IsError)
+                {
+                    Console.WriteLine(tokenResponce.Error);
+                    return;
+                }
+                Console.WriteLine(tokenResponce.Json);
+                using(var apiClient = new HttpClient())
+                {
+                    apiClient.SetBearerToken(tokenResponce.AccessToken);
+                    var responce = await apiClient.GetAsync("https://localhost:6001/identity");
+                    if(!responce.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine(responce.StatusCode);
+                    }
+                    else
+                    {
+                        var content = await responce.Content.ReadAsStringAsync();
+                        Console.WriteLine(JArray.Parse(content));
+                    }
+                }
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
