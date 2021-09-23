5c3492a4c2fbb3e003faed5234678bc225cbfcf8
Akhmadjon <formyproject1999@gmail.com>
2021-09-23 11:22:59 +0500

    Add Source

---
+++ IdentityServer4Configuration/Models/SysUserToken.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4Configuration.Models
+{
+    public class SysUserToken : IdentityUserToken<Guid>
+    {
+        public virtual SysUsers User { get; set; }
+    }
+}
---
+++ IdentityServer4Configuration/Models/SysUsers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4Configuration.Models
+{
+    public class SysUsers : IdentityUser<Guid>
+    {
+        public virtual ICollection<SysUserClaim> Claims { get; set; }
+
+        public virtual ICollection<SysUserLogin> Logins { get; set; }
+
+        public virtual ICollection<SysUserToken> Tokens { get; set; }
+
+        public virtual ICollection<SysUserRole> Roles { get; set; }
+    }
+}
